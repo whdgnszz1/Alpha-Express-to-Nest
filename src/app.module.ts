6993551b3a09f00d7fa08c5e0653c7b1b7e6f678
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-23 20:38:10 +0900

    Setting: Setting config to use env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { PrismaModule } from './prisma.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env.develpoment' }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
