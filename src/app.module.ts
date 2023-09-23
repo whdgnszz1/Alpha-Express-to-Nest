60d8b0121bf1c61b31802cf5db1c5408137be071
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-23 20:41:10 +0900

    Setting: Setting logging middleware for all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { PrismaModule } from './prisma.module';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
   imports: [
@@ -12,4 +13,8 @@ import { ConfigModule } from '@nestjs/config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
