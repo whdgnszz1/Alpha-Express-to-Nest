f205c559ca7e5c43f23c4b5fb976ed7a7b2fc0e9
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-23 20:45:57 +0900

    Feat: Add undefinedToNull interceptor for project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { UndefinedToNullInterceptor } from './common/interceptors/undefinedToNullI.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new UndefinedToNullInterceptor());
   const PORT = process.env.PORT || 8000;
   await app.listen(PORT);
 }
