880b44a7fbb26dfd68cb0c57c1268ab7f96a83b1
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-23 20:49:09 +0900

    Feat: Add http-exception filter for project

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { UndefinedToNullInterceptor } from './common/interceptors/undefinedToNullI.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new UndefinedToNullInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
   const PORT = process.env.PORT || 8000;
   await app.listen(PORT);
 }
