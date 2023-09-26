7ed9232470454267717032ee0d637555c8d136da
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-26 18:44:55 +0900

    Feat: Dependency inject to boards module

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
+import { PrismaModule } from 'src/prisma.module';
+import { BoardsRepository } from './boards.repository';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [BoardsController],
-  providers: [BoardsService],
+  providers: [BoardsService, BoardsRepository],
 })
 export class BoardsModule {}
