d54733c4f376d7a7834b6dc408eb8aea804ce320
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-26 18:01:02 +0900

    Setting: Setting swagger for project

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { SuccessInterceptor } from './common/interceptors/success.interceptor';
@@ -6,6 +7,16 @@ import { UndefinedToNullInterceptor } from './common/interceptors/undefinedToNul
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Alpha')
+    .setDescription('Alpha API doc')
+    .setVersion('1.0')
+    .addTag('alpha')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   app.useGlobalInterceptors(
     new UndefinedToNullInterceptor(),
     new SuccessInterceptor(),
